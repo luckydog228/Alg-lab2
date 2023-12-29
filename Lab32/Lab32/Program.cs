1e760e0b9b300b78ee417cc737110cce6c669eda
luckydog228 <128289395+luckydog228@users.noreply.github.com>
2023-12-29 18:44:21 +0300

    Add files via upload

---
+++ Lab32/Lab32/Program.cs
@@ -0,0 +1,164 @@
+﻿using System;
+
+public class ClassRoom
+{
+    private Pupil[] pupils;
+
+    public ClassRoom(params Pupil[] pupils)
+    {
+        if (pupils.Length != 2 && pupils.Length != 3 && pupils.Length != 4)
+        {
+            throw new ArgumentException("Wrong number of pupils");
+        }
+        this.pupils = pupils;
+    }
+
+    public void PrintClassInfo()
+    {
+        Console.WriteLine("Classroom composition:");
+        for (int i = 0; i < pupils.Length; i++)
+        {
+            Console.WriteLine($"Pupil {i + 1}: {pupils[i].GetType().Name}");
+        }
+    }
+
+    public void Study()
+    {
+        foreach (var pupil in pupils)
+        {
+            pupil.Study();
+        }
+    }
+
+    public void Read()
+    {
+        foreach (var pupil in pupils)
+        {
+            pupil.Read();
+        }
+    }
+
+    public void Write()
+    {
+        foreach (var pupil in pupils)
+        {
+            pupil.Write();
+        }
+    }
+
+    public void Relax()
+    {
+        foreach (var pupil in pupils)
+        {
+            pupil.Relax();
+        }
+    }
+}
+
+public class Pupil
+{
+    public virtual void Study()
+    {
+        Console.WriteLine("Pupil is studying");
+    }
+
+    public virtual void Read()
+    {
+        Console.WriteLine("Pupil is reading");
+    }
+
+    public virtual void Write()
+    {
+        Console.WriteLine("Pupil is writing");
+    }
+
+    public virtual void Relax()
+    {
+        Console.WriteLine("Pupil is relaxing");
+    }
+}
+
+public class ExcellentPupil : Pupil
+{
+    public override void Study()
+    {
+        Console.WriteLine("Excellent pupil is studying");
+    }
+
+    public override void Read()
+    {
+        Console.WriteLine("Excellent pupil is reading");
+    }
+
+    public override void Write()
+    {
+        Console.WriteLine("Excellent pupil is writing");
+    }
+
+    public override void Relax()
+    {
+        Console.WriteLine("Excellent pupil is relaxing");
+    }
+}
+
+public class GoodPupil : Pupil
+{
+    public override void Study()
+    {
+        Console.WriteLine("Good pupil is studying");
+    }
+
+    public override void Read()
+    {
+        Console.WriteLine("Good pupil is reading");
+    }
+
+    public override void Write()
+    {
+        Console.WriteLine("Good pupil is writing");
+    }
+
+    public override void Relax()
+    {
+        Console.WriteLine("Good pupil is relaxing");
+    }
+}
+
+public class BadPupil : Pupil
+{
+    public override void Study()
+    {
+        Console.WriteLine("Bad pupil is studying");
+    }
+
+    public override void Read()
+    {
+        Console.WriteLine("Bad pupil is reading");
+    }
+
+    public override void Write()
+    {
+        Console.WriteLine("Bad pupil is writing");
+    }
+
+    public override void Relax()
+    {
+        Console.WriteLine("Bad pupil is relaxing");
+    }
+}
+
+ public class Program
+{
+    public static void Main(string[] args)
+    {
+        ClassRoom classRoom = new ClassRoom(new ExcellentPupil(), new GoodPupil(), new BadPupil());
+        classRoom.PrintClassInfo();
+
+        Console.WriteLine("\nStudents are:");
+
+        classRoom.Study();
+        classRoom.Read();
+        classRoom.Write();
+        classRoom.Relax();
+    }
+}
---
+++ Lab32/Lab322/Program.cs
@@ -0,0 +1,93 @@
+﻿using System;
+
+class Vehicle
+{
+    protected double x, y;
+    protected double price, speed;
+    protected int year;
+
+    public Vehicle(double x, double y, double price, double speed, int year)
+    {
+        this.x = x;
+        this.y = y;
+        this.price = price;
+        this.speed = speed;
+        this.year = year;
+    }
+
+    public void DisplayInfo()
+    {
+        Console.WriteLine("Coordinates: ({0}, {1})", x, y);
+        Console.WriteLine("Price: {0}", price);
+        Console.WriteLine("Speed: {0}", speed);
+        Console.WriteLine("Year: {0}", year);
+    }
+}
+
+class Plane : Vehicle
+{
+    private double height;
+    private int passengers;
+
+    public Plane(double x, double y, double price, double speed, int year, double height, int passengers)
+    : base(x, y, price, speed, year)
+    {
+        this.height = height;
+        this.passengers = passengers;
+    }
+
+    public new void DisplayInfo()
+    {
+        base.DisplayInfo();
+        Console.WriteLine("Height: {0}", height);
+        Console.WriteLine("Passengers: {0}", passengers);
+    }
+}
+
+class Car : Vehicle
+{
+    public Car(double x, double y, double price, double speed, int year)
+    : base(x, y, price, speed, year)
+    {
+    }
+}
+
+class Ship : Vehicle
+{
+    private int passengers;
+    private string port;
+
+    public Ship(double x, double y, double price, double speed, int year, int passengers, string port)
+    : base(x, y, price, speed, year)
+    {
+        this.passengers = passengers;
+        this.port = port;
+    }
+
+    public new void DisplayInfo()
+    {
+        base.DisplayInfo();
+        Console.WriteLine("Passengers: {0}", passengers);
+        Console.WriteLine("Port: {0}", port);
+    }
+}
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Plane plane = new Plane(100, 200, 1000000, 700, 2020, 10000, 200);
+        Console.WriteLine("Plane info:");
+        plane.DisplayInfo();
+
+        Car car = new Car(300, 400, 50000, 200, 2018);
+        Console.WriteLine("\nCar info:");
+        car.DisplayInfo();
+
+        Ship ship = new Ship(500, 600, 2000000, 50, 2015, 1000, "New York");
+        Console.WriteLine("\nShip info:");
+        ship.DisplayInfo();
+
+        Console.ReadLine();
+    }
+}
